bdedec38fb3b5c69affce4d89c1d0347e2e4877b
Karol Przybysz <przybyszkarol97@gmail.com>
2026-08-20 13:19:44 +0200

    add Api settings

---
+++ DMS/Configuration/ApiSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMS.Configuration
+{
+	internal static class ApiSettings
+	{
+		internal static string BaseUrl
+		{
+			get
+			{
+				string value =
+					ConfigurationManager.AppSettings["ApiBaseUrl"];
+
+				if (string.IsNullOrWhiteSpace(value))
+				{
+					throw new ConfigurationErrorsException(
+						"Brak Url w config");
+				}
+
+				return value;
+			}
+		}
+	}
+}
