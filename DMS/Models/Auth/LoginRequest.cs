86d83cd0c55a2283ad2a79341f3022fe54b1755a
Karol Przybysz <przybyszkarol97@gmail.com>
2026-08-23 22:03:16 +0200

    Add authentication request and response models

---
+++ DMS/Models/Auth/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DMS.Models.Auth
+{
+	internal class LoginRequest
+	{
+		public string Email { get; set; }
+		public string Password { get; set; }
+	}
+}
---
+++ DMS/Models/Auth/LoginResult.cs
@@ -0,0 +1,8 @@
+﻿namespace DMS.Models.Auth
+{
+	internal class LoginResult
+	{
+		public string AccessToken { get; set; }
+		public string RefreshToken { get; set; }
+	}
+}
