416e11d2c20a6c2343fb2926dbfb43f2a357bffd
Karol Przybysz <przybyszkarol97@gmail.com>
2026-08-23 22:03:41 +0200

    Add user session

---
+++ DMS/Session/UserSession.cs
@@ -0,0 +1,23 @@
+﻿namespace DMS.Session
+{
+	internal class UserSession
+	{
+		public string AccessToken { get; set; }
+		public string RefreshToken { get; set; }
+		public string Email { get; set; }
+		public bool IsLoggedIn => !string.IsNullOrWhiteSpace(AccessToken);	
+		public void Start(string accessToken, string refreshToken, string email)
+		{
+			AccessToken = accessToken;
+			RefreshToken = refreshToken;
+			Email = email;
+		}
+
+		public void Clear()
+		{
+			AccessToken = null;
+			RefreshToken = null;
+			Email = null;
+		}
+	}
+}
