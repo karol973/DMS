459b8f86453ec78e5ee8184761ede2420ead8021
Karol Przybysz <przybyszkarol97@gmail.com>
2026-08-23 22:01:57 +0200

    Add service base

---
+++ DMS/Services/ServiceBase.cs
@@ -0,0 +1,11 @@
+﻿namespace DMS.Services
+{
+	internal abstract class ServiceBase
+	{
+		protected ApiClient _apiClient { get; }
+		protected ServiceBase(ApiClient apiClient)
+		{
+			_apiClient = apiClient;
+		}
+	}
+}
